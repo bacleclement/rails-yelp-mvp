37cfcf3701145d6b7abf98e0c5f7f6dbee2a5e50
clement bacle <clement@officinasante.fr>
2017-11-22 12:44:17 +0100

    adding restaurants and reviews controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+
+  get 'reviews/index'
+
+  get 'reviews/new'
+
+  get 'reviews/create'
+
+  resources :restaurants, only: [:index, :show, :new, :create]
+
+  # root 'restaurants' to:
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+5.times do
+  Restaurant.create!(
+    name: Faker::StarWars.character,
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: CATEGORIES[rand(0..4)]
+    )
+end
